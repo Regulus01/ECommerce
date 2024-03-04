9f439a932d52061d557f50ed436e594d6e9afc12
josePc <jose.csousa22@gmail.com>
2024-03-03 22:06:22 -0300

    feat: Restrição de acesso

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,22 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
 
+// Verifica se o agente do usuário indica que é um dispositivo móvel
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Verifica se é um dispositivo móvel e redireciona ou mostra uma mensagem de erro
+if (isMobileDevice()) {
+    // Redireciona para uma página de erro, por exemplo:
+    // window.location.href = "pagina-de-erro.html";
+    // Ou exibe uma mensagem de erro
+    alert("Este aplicativo não pode ser acessado em dispositivos móveis.");
+    window.location.href = "https://www.google.com";
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+    
     <AuthProvider>
         <App />
     </AuthProvider>
